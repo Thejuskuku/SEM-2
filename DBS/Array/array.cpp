e4544aca211aef682f43fe16cff194e7305b5976
Thejuskuku <143246271+Thejuskuku@users.noreply.github.com>
2024-02-04 23:40:33 +0530

    Add files via upload

---
+++ DBS/Array/array.cpp
@@ -0,0 +1,41 @@
+#include"Array.h"
+
+
+template <class T>
+Array<T>::Array(){
+	LB=1;
+	UB=0;
+}
+template <class T>
+Array<T>::Array(int LB,int UB,T x[]){
+	
+}
+template <class T>
+void Array<T>::insert_at_end(T key){
+	UB=UB+1;
+	A[UB]=key;
+}
+template <class U>
+ostream& operator << (ostream& os,Array<U> M){
+	int i;
+	os<<endl;
+	for (i=M.LB;i<=M.UB;i++){
+		os<<M.A[i]<<" ";
+	}
+	os<<endl;
+	return os;
+}
+int main(){
+	Array<int> myArray;
+	myArray.insert_at_end(3);
+	myArray.insert_at_end(5);
+	myArray.insert_at_end(6);
+	cout<<myArray;
+	
+	Array<float> myfArray;
+	myfArray.insert_at_end(3.5);
+	myfArray.insert_at_end(5.5);
+	myfArray.insert_at_end(6.3);
+	cout<<myfArray;
+	return 0;
+}
